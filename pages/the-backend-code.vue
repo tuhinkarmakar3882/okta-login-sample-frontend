18c268b0057c9494f5fd2cdf53279290b23eeaf1
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-10-19 16:52:17 +0530

    Fixed Base URL
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- pages/the-backend-code.vue
+++ pages/the-backend-code.vue
@@ -32,7 +32,7 @@ def login_callback(request, *args, **kwargs):
     client_id = 'your-okta-client-id-goes-here'
     domain_url = 'your-okta-domain-url-goes-here'
 
-    frontend_redirection_url = "http://localhost:3000/login/callback"
+    frontend_redirection_url = "http://{{FRONTEND_URL}}/login/callback"
     okta_auth_endpoint = "https://{}/oauth2/default/v1/token".format(domain_url)
     backend_redirect_uri = 'http://localhost:8000/login/callback/'
 
@@ -107,50 +107,7 @@ export default {
   data() {
     return {
       pageTitle: 'The Backend Code',
-      pythonCodeSnippet: {
-        djangoView: `
-import requests
-from django.http import HttpResponseRedirect, JsonResponse
-from requests.auth import HTTPBasicAuth
-
-def login_callback(request, *args, **kwargs):
-    okta_code = request.GET.get('code')
-
-    if okta_code is None:
-        return JsonResponse({"error": "The Okta Code is None"})
-
-    client_secret = 'your-okta-client-secret-goes-here'
-    client_id = 'your-okta-client-id-goes-here'
-    domain_url = 'your-okta-domain-url-goes-here'
-    frontend_redirection_url = "http://localhost:3000/login/callback"
-    okta_auth_endpoint = "https://{}/oauth2/default/v1/token".format(domain_url)
-
-    response = dict(requests.post(
-        url=okta_auth_endpoint,
-        auth=HTTPBasicAuth(client_id, client_secret),
-        headers={
-            'accept': 'application/json',
-            'content-type': 'application/x-www-form-urlencoded',
-        },
-        data={
-            'grant_type': 'authorization_code',
-            'redirect_uri': 'http://localhost:8000/login/callback/',
-            'code': okta_code
-        }
-    ).json())
-
-    redirect_endpoint = "{}?token_type={}&expires_in={}&access_token={}&scope={}&id_token={}".format(
-        frontend_redirection_url,
-        response.get('token_type'),
-        response.get('expires_in'),
-        response.get('access_token'),
-        response.get('scope'),
-        response.get('id_token'),
-    )
-
-    return HttpResponseRedirect(redirect_endpoint)
-        `,
-      },
+      FRONTEND_URL: window.location.host,
     }
   },
 
