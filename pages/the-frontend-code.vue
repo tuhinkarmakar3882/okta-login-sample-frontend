10c883880dcd3f5cab5d5c1208df174f8039f9c7
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-10-19 15:07:12 +0530

    Added the frontend half
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- pages/the-frontend-code.vue
+++ pages/the-frontend-code.vue
@@ -7,12 +7,57 @@
     </header>
 
     <main>
-      <h3>Let's install the JavaScript Package</h3>
-      <pre><code class="language-xml">
-&lt;script src="https://global.oktacdn.com/okta-auth-js/4.5.0/okta-auth-js.min.js" type="text/javascript"&gt;&lt;/script&gt;
-      </code></pre>
-      <p>OR</p>
-      <pre><code class="language-bash">yarn add @okta/okta-auth-js  </code></pre>
+      <section>
+        <h3>Let's install the JavaScript Package</h3>
+        <pre><code class="language-xml">&lt;script src="https://global.oktacdn.com/okta-auth-js/4.5.0/okta-auth-js.min.js" type="text/javascript"&gt;&lt;/script&gt;</code></pre>
+        <p>OR</p>
+        <pre><code class="language-bash">yarn add @okta/okta-auth-js</code></pre>
+      </section>
+
+      <section>
+        <h3>Create a Button</h3>
+        <pre><code class="language-xml">&lt;button id="login-button"&gt;Login with Okta&lt;/button&gt;</code></pre>
+      </section>
+
+      <section>
+        <h3>Set up the OKTA Auth Client</h3>
+        <pre><code class="language-js">//filename: oktaAuthClient.js
+import { OktaAuth } from '@okta/okta-auth-js'
+
+const authServerId = 'default'
+const yourOktaDomain = 'your-okta-domain'
+const clientId = 'clientId'
+const redirectUri = 'redirectUri'
+
+export const oktaAuthClient = new OktaAuth({
+  url: `https://${yourOktaDomain}`,
+  clientId,
+  redirectUri,
+  responseType: 'token',
+  pkce: false,
+  issuer: `https://${yourOktaDomain}/oauth2/${authServerId}`,
+})
+</code></pre>
+      </section>
+
+      <section>
+        <h3>Add the JavaScript</h3>
+        <pre><code class="language-js">//filename: main.js
+import { oktaAuthClient } from './oktaAuthClient.js'
+
+const loginButton = document.getElementById('login-button');
+
+loginButton.addEventListener('click', async () => {
+  await oktaAuthClient.token.getWithRedirect({
+    responseType: 'code',
+  })
+})</code></pre>
+      </section>
+
+      <p>
+        So, when the button will be clicked, it will take you to okta login page
+        and the okta login will send you back to the backend login callback
+      </p>
     </main>
 
     <footer>
